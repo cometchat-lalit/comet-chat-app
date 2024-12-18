7fd1425613a246fcb9d7ca97f46d5124d7fe47ba
lalit <lalit.vind@cometchat.com>
2024-12-18 16:45:07 +0530

    Initial Commit

---
+++ comet-chat-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  comet-chat-app
+//
+//  Created by Admin on 12/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ comet-chat-appTests/comet_chat_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  comet_chat_appTests.swift
+//  comet-chat-appTests
+//
+//  Created by Admin on 12/18/24.
+//
+
+import Testing
+@testable import comet_chat_app
+
+struct comet_chat_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
